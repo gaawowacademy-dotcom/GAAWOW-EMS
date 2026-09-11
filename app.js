8b2f0d0e726cba09e7235f2749a80f3de40b69c6
gaawowacademy-dotcom <gaawow.academy@gmail.com>
2026-09-11 16:52:18 +0300

    Update Supabase URL in app.js
    Add Supabase login connection

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ==========================================
 
 const SUPABASE_URL =
-  "https://mujmcunlmkvtbjslaely.supabase.co";
+  "https://mytyvqwrxnxpxnxpiicj.supabase.co";
 
 const SUPABASE_KEY =
   "sb_publishable_2AvWfupkF1b_s0RjIbAi5g_RqLCs145";
