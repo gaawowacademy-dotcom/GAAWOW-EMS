12b5dbba0a7801ec858612cb28a7b66e4358fc06
gaawowacademy-dotcom <gaawow.academy@gmail.com>
2026-09-12 01:49:31 +0300

    Update Supabase key in app.js

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ const SUPABASE_URL =
   "https://mytyvqwrxnxpxnxpiicj.supabase.co";
 
 const SUPABASE_KEY =
-  "sb_publishable_2AvWfupkKf1b_s0RjIbAi5g_RqLCs145";
-
+  "sb_publishable_2AvWfupkF1b_s0RjIbAi5g_RqLCs145";
 const db =
   window.supabase.createClient(
     SUPABASE_URL,
