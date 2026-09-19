6f448577bd2134bb0567a93b0ae87c79b21d6151
gaawowacademy-dotcom <gaawow.academy@gmail.com>
2026-09-20 00:15:25 +0300

    Fix SUPABASE_URL and SUPABASE_KEY formatting
    Corrected SUPABASE_KEY formatting and removed extra character.

--- students.js
+++ students.js
@@ -11,10 +11,10 @@
    ========================================================= */
 
 const SUPABASE_URL =
-  "https://mytyvqwrxnxpxnxpiicj.supabase.co";
+  "https://mytyvqwrxnxpxnxpiicj.supabase.co;
 
 const SUPABASE_KEY =
-  "sb_publishable_2AvWfupKf1b_s0RjIbAi5g_RqLCs145";
+  "sb_publishable_2AvWfupkF1b_s0RjIbAi5g_RqLCs145";
 
 const PHOTO_BUCKET = "student-photos";
 
